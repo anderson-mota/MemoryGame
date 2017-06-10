ff1f4cdc3a47b322dbed20192e18296442747e57
Anderson Mota <anderson.mota12@gmail.com>
2017-06-10 00:50:49 -0300

    Opção para reiniciar o jogo.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,23 +1,6 @@
 angular.module('memoryApp', [])
     .controller('CardsController', function($scope) {
-        $scope.TurnedCards = [];
-        $scope.cards = [
-            {hit: false, id: 1, show: false, image: 'bulbasaur.png'},
-            {hit: false, id: 2, show: false, image: 'butterfree.png'},
-            {hit: false, id: 3, show: false, image: 'quirtle.png'},
-            {hit: false, id: 4, show: false, image: 'charmander.png'},
-            {hit: false, id: 5, show: false, image: 'eevee.png'},
-            {hit: false, id: 6, show: false, image: 'haunter.png'},
-            {hit: false, id: 7, show: false, image: 'horsea.png'},
-            {hit: false, id: 8, show: false, image: 'meowth.png'},
-            {hit: false, id: 9, show: false, image: 'nidorina.png'},
-            {hit: false, id: 10, show: false, image: 'pidgey.png'},
-            {hit: false, id: 11, show: false, image: 'pikachu.png'},
-            {hit: false, id: 12, show: false, image: 'sandshrew.png'},
-            {hit: false, id: 13, show: false, image: 'tauros.png'},
-            {hit: false, id: 14, show: false, image: 'togepi.png'},
-            {hit: false, id: 15, show: false, image: 'vulpix.png'},
-            {hit: false, id: 16, show: false, image: 'lugia.png'},
+        var cards = [
             {hit: false, id: 1, show: false, image: 'bulbasaur.png'},
             {hit: false, id: 2, show: false, image: 'butterfree.png'},
             {hit: false, id: 3, show: false, image: 'quirtle.png'},
@@ -35,6 +18,20 @@ angular.module('memoryApp', [])
             {hit: false, id: 15, show: false, image: 'vulpix.png'},
             {hit: false, id: 16, show: false, image: 'lugia.png'}
         ];
+        $scope.TurnedCards = [];
+        $scope.cards = [];
+        $scope.actionButton = "Iniciar";
+
+        $scope.resetGame = function() {
+            $scope.actionButton = "Reiniciar";
+            var _cards = angular.copy(cards);
+            $scope.cards = _.flatMap(_cards, function(card) {
+                var _copy = angular.copy(card);
+                return [card, _copy];
+            });
+
+            $scope.shuffleCards();
+        };
 
         $scope.shuffleCards = function() {
             $scope.cards = _.shuffle($scope.cards);
@@ -68,6 +65,4 @@ angular.module('memoryApp', [])
                 $scope.TurnedCards.push($card);
             }
         };
-
-        $scope.shuffleCards();
     });
