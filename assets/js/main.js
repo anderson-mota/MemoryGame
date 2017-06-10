fa6d0c75d248f283f6250b01b91f3d0cf19fad85
Anderson Mota <anderson.mota12@gmail.com>
2017-06-10 00:24:37 -0300

    Logica do jogo implementada.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 angular.module('memoryApp', [])
     .controller('CardsController', function($scope) {
-        $scope.backCard = "_pokebola.jpg";
+        $scope.TurnedCards = [];
         $scope.cards = [
             {hit: false, id: 1, show: false, image: 'bulbasaur.png'},
             {hit: false, id: 2, show: false, image: 'butterfree.png'},
@@ -12,11 +12,12 @@ angular.module('memoryApp', [])
             {hit: false, id: 8, show: false, image: 'meowth.png'},
             {hit: false, id: 9, show: false, image: 'nidorina.png'},
             {hit: false, id: 10, show: false, image: 'pidgey.png'},
-            {hit: true, id: 11, show: true, image: 'pikachu.png'},
+            {hit: false, id: 11, show: false, image: 'pikachu.png'},
             {hit: false, id: 12, show: false, image: 'sandshrew.png'},
             {hit: false, id: 13, show: false, image: 'tauros.png'},
             {hit: false, id: 14, show: false, image: 'togepi.png'},
             {hit: false, id: 15, show: false, image: 'vulpix.png'},
+            {hit: false, id: 16, show: false, image: 'lugia.png'},
             {hit: false, id: 1, show: false, image: 'bulbasaur.png'},
             {hit: false, id: 2, show: false, image: 'butterfree.png'},
             {hit: false, id: 3, show: false, image: 'quirtle.png'},
@@ -27,21 +28,44 @@ angular.module('memoryApp', [])
             {hit: false, id: 8, show: false, image: 'meowth.png'},
             {hit: false, id: 9, show: false, image: 'nidorina.png'},
             {hit: false, id: 10, show: false, image: 'pidgey.png'},
-            {hit: true, id: 11, show: true, image: 'pikachu.png'},
+            {hit: false, id: 11, show: false, image: 'pikachu.png'},
             {hit: false, id: 12, show: false, image: 'sandshrew.png'},
             {hit: false, id: 13, show: false, image: 'tauros.png'},
             {hit: false, id: 14, show: false, image: 'togepi.png'},
-            {hit: false, id: 15, show: false, image: 'vulpix.png'}
+            {hit: false, id: 15, show: false, image: 'vulpix.png'},
+            {hit: false, id: 16, show: false, image: 'lugia.png'}
         ];
-
 
         $scope.shuffleCards = function() {
             $scope.cards = _.shuffle($scope.cards);
         };
 
         $scope.turnCard = function($card) {
-            if (!$card.hit) {
-                $card.show = !$card.show;
+            if ($card.hit || $card.show) {
+                return;
+            }
+
+            if ($scope.TurnedCards.length >= 2) {
+                angular.forEach($scope.TurnedCards, function($card) {
+                    $card.show = false;
+                });
+
+                $scope.TurnedCards = [];
+            }
+
+            $card.show = !$card.show;
+
+            if ($scope.TurnedCards.length == 1) {
+                var turnedCard = $scope.TurnedCards[0];
+                if (turnedCard.id == $card.id) {
+                    turnedCard.hit = $card.hit = true;
+                    $scope.TurnedCards = [];
+                    return;
+                }
+            }
+
+            if ($card.show) {
+                $scope.TurnedCards.push($card);
             }
         };
 
