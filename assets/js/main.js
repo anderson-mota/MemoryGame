9f1de21b709d8c20dca5db197006b619c6034a38
Anderson Mota <anderson.mota12@gmail.com>
2017-06-10 01:38:07 -0300

    Criado contagem e refatoração de código.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,68 +1,80 @@
 angular.module('memoryApp', [])
     .controller('CardsController', function($scope) {
-        var cards = [
-            {hit: false, id: 1, show: false, image: 'bulbasaur.png'},
-            {hit: false, id: 2, show: false, image: 'butterfree.png'},
-            {hit: false, id: 3, show: false, image: 'quirtle.png'},
-            {hit: false, id: 4, show: false, image: 'charmander.png'},
-            {hit: false, id: 5, show: false, image: 'eevee.png'},
-            {hit: false, id: 6, show: false, image: 'haunter.png'},
-            {hit: false, id: 7, show: false, image: 'horsea.png'},
-            {hit: false, id: 8, show: false, image: 'meowth.png'},
-            {hit: false, id: 9, show: false, image: 'nidorina.png'},
-            {hit: false, id: 10, show: false, image: 'pidgey.png'},
-            {hit: false, id: 11, show: false, image: 'pikachu.png'},
-            {hit: false, id: 12, show: false, image: 'sandshrew.png'},
-            {hit: false, id: 13, show: false, image: 'tauros.png'},
-            {hit: false, id: 14, show: false, image: 'togepi.png'},
-            {hit: false, id: 15, show: false, image: 'vulpix.png'},
-            {hit: false, id: 16, show: false, image: 'lugia.png'}
-        ];
+        var cards = ['bulbasaur.png', 'butterfree.png', 'quirtle.png', 'charmander.png', 'eevee.png', 'haunter.png',
+            'horsea.png', 'meowth.png', 'nidorina.png', 'pidgey.png', 'pikachu.png', 'sandshrew.png', 'tauros.png',
+            'togepi.png', 'vulpix.png', 'lugia.png'];
+        var concludedCards = [];
         $scope.TurnedCards = [];
         $scope.cards = [];
-        $scope.actionButton = "Iniciar";
+        $scope.games = 0;
 
         $scope.resetGame = function() {
-            $scope.actionButton = "Reiniciar";
-            var _cards = angular.copy(cards);
-            $scope.cards = _.flatMap(_cards, function(card) {
-                var _copy = angular.copy(card);
-                return [card, _copy];
+            $scope.cards = _.flatMap(cards, function(cardImage, index) {
+                var card = {hit: false, id: ++index, show: false, image: cardImage};
+                var _card = angular.copy(card);
+                return [card, _card];
             });
 
             $scope.shuffleCards();
+            $scope.games = 0;
         };
 
         $scope.shuffleCards = function() {
             $scope.cards = _.shuffle($scope.cards);
         };
 
-        $scope.turnCard = function($card) {
+        $scope.turnDown = function() {
+            angular.forEach($scope.TurnedCards, function($card) {
+                $card.show = false;
+            });
+
+            $scope.TurnedCards = [];
+        };
+
+        $scope.compareCards = function($card) {
+            if ($scope.TurnedCards.length != 1) {
+                return false;
+            }
+
+            var turnedCard = $scope.TurnedCards[0];
+            if (turnedCard.id != $card.id) {
+                return false;
+            }
+
+            turnedCard.hit = $card.hit = true;
+            $scope.TurnedCards = [];
+            concludedCards.push($card.id);
+
+            if (concludedCards.length == cards.length) {
+                alert("Parabêns você ganhou!");
+                $scope.resetGame();
+            }
+
+            return true;
+        };
+
+        $scope.turnCard = function($card, event) {
+            event.preventDefault();
+
             if ($card.hit || $card.show) {
                 return;
             }
 
             if ($scope.TurnedCards.length >= 2) {
-                angular.forEach($scope.TurnedCards, function($card) {
-                    $card.show = false;
-                });
-
-                $scope.TurnedCards = [];
+                $scope.turnDown();
             }
 
             $card.show = !$card.show;
+            $scope.games++;
 
-            if ($scope.TurnedCards.length == 1) {
-                var turnedCard = $scope.TurnedCards[0];
-                if (turnedCard.id == $card.id) {
-                    turnedCard.hit = $card.hit = true;
-                    $scope.TurnedCards = [];
-                    return;
-                }
+            if ($scope.compareCards($card)) {
+                return;
             }
 
             if ($card.show) {
                 $scope.TurnedCards.push($card);
             }
         };
+
+        $scope.resetGame();
     });
